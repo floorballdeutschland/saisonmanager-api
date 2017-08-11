7473b6b426a5cb8537c959b3ec3584246a261b8c
Jan Hoffmann <jho@jhoit.de>
2017-08-12 01:15:48 +0200

    add authentication to all controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  private
+  def authenticate_user
+    token = (request.headers['Authorization'] || '').split(' ').last
+    render json: { success: false, message: 'Not authenticated' }, status: 401 unless User.check_token token
+  end
 end
