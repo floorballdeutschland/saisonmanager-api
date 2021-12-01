b50cd4154a578b0dc2d4c4fcdb128f6bc1be63ad
Jan Hoffmann <jho@jhoit.de>
2021-12-01 19:16:05 +0100

    add top_leagues_stub (see floorballdeutschland/saisonmanager#12)

--- app/models/game_operation.rb
+++ app/models/game_operation.rb
@@ -7,7 +7,13 @@ class GameOperation < ApplicationRecord
     leagues.map(&:games).flatten.sort_by{|i| i.game_number.to_i}
   end
 
+  def top_league_ids
+    leagues.pluck(:id).sample(3)
+  end
+
   def short_hash
-    attributes.with_indifferent_access.slice(:id, :name, :short_name, :path, :logo_url, :logo_quad_url)
+    result = attributes.with_indifferent_access.slice(:id, :name, :short_name, :path, :logo_url, :logo_quad_url)
+    result[:top_league_ids] = top_league_ids
+    result
   end
 end
