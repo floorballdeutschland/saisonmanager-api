e7108e805927fa190c91f50fadadc8a8f3e13723
Jan Hoffmann <jho@jhoit.de>
2022-08-06 17:23:22 +0200

    add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
 
   private
   def authenticate_user
+    @user = current_user
+    render json: { success: false, message: 'Not authenticated' }, status: 401 unless @user
+  end
+
+  def current_user
     user_id = cookies.signed[:user_id]
-    @user = User.find_by_id user_id if user_id
-    render json: { success: false, message: 'Not authenticated' }, status: 401 unless user_id
+    User.find_by_id user_id if user_id
   end
 end
