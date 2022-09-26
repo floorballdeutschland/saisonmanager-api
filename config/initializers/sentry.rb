9b3cc3604ff8fd635e2c93181d0f29946cd557b0
Jan Hoffmann <jho@jhoit.de>
2022-09-26 16:55:30 +0200

    disable sentry on dev

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = 'https://85b4823c49024668be39942d31c3fc21@o1082542.ingest.sentry.io/6091175'
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.enabled_environments = %w[production staging]
 
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
