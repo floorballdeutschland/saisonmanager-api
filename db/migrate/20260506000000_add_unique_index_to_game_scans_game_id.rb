f7d134766aefff7d106075c824dbee881f4d8cf8
Daniel Kehne <daniel.kehne@web.de>
2026-05-06 10:27:32 +0200

    fix: Migration ersetzt bestehenden nicht-uniquen Index auf game_scans.game_id

--- db/migrate/20260506000000_add_unique_index_to_game_scans_game_id.rb
+++ db/migrate/20260506000000_add_unique_index_to_game_scans_game_id.rb
@@ -1,5 +1,11 @@
 class AddUniqueIndexToGameScansGameId < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    remove_index :game_scans, :game_id if index_exists?(:game_scans, :game_id)
     add_index :game_scans, :game_id, unique: true
+  end
+
+  def down
+    remove_index :game_scans, :game_id
+    add_index :game_scans, :game_id
   end
 end
