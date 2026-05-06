7461f6cb0ee8746cf742d3c9ecedf50d87e2d203
Daniel Kehne <daniel.kehne@web.de>
2026-05-06 10:26:20 +0200

    fix: doppelter game_id-Index in CreateGameRefereeReports-Migration

--- db/migrate/20260505210003_create_game_referee_reports.rb
+++ db/migrate/20260505210003_create_game_referee_reports.rb
@@ -1,7 +1,7 @@
 class CreateGameRefereeReports < ActiveRecord::Migration[7.0]
   def change
     create_table :game_referee_reports do |t|
-      t.references :game, null: false, foreign_key: true
+      t.references :game, null: false, foreign_key: true, index: false
       t.references :uploaded_by, foreign_key: { to_table: :users }, null: false
       t.timestamps
     end
