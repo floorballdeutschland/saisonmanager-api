ce71cb2fc211c3f5b7730a704df904271ba3e850
Daniel Kehne <daniel.kehne@web.de>
2026-05-15 17:59:27 +0100

    chore: sm.jholocal.de entfernen, schema.rb auf Migration 20260521100000 aktualisieren

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_20_200000) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_21_100000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -244,6 +244,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_20_200000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "direct_comparison", default: false, null: false
+    t.string "required_documents", default: [], array: true
     t.index ["game_operation_id"], name: "index_leagues_on_game_operation_id"
   end
 
