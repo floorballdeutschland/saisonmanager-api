c2af2a11e56e0b56ba648a6b4d4f2a43817559f5
Daniel Kehne <daniel.kehne@web.de>
2026-05-24 13:47:21 +0200

    fix: RuboCop in referee_test.rb (NumericLiterals, UselessAssignment)

--- test/models/referee_test.rb
+++ test/models/referee_test.rb
@@ -11,8 +11,8 @@ class RefereeTest < ActiveSupport::TestCase
   end
 
   test 'partner_lizenznummer: setzt Gegen-Eintrag beim Partner, wenn dort noch leer' do
-    a = make_referee(lizenznummer: 20001)
-    b = make_referee(lizenznummer: 20002)
+    a = make_referee(lizenznummer: 20_001)
+    b = make_referee(lizenznummer: 20_002)
 
     a.update!(partner_lizenznummer: b.lizenznummer)
 
@@ -21,27 +21,26 @@ class RefereeTest < ActiveSupport::TestCase
   end
 
   test 'partner_lizenznummer: überschreibt bestehenden Partner-Eintrag NICHT' do
-    a = make_referee(lizenznummer: 20003)
-    b = make_referee(lizenznummer: 20004)
-    c = make_referee(lizenznummer: 20005, partner_lizenznummer: 99999)
+    a = make_referee(lizenznummer: 20_003)
+    c = make_referee(lizenznummer: 20_005, partner_lizenznummer: 99_999)
 
     a.update!(partner_lizenznummer: c.lizenznummer)
 
     assert_equal c.lizenznummer, a.reload.partner_lizenznummer
-    assert_equal 99999, c.reload.partner_lizenznummer
+    assert_equal 99_999, c.reload.partner_lizenznummer
   end
 
   test 'partner_lizenznummer: kein Fehler wenn Partner-Lizenznummer nicht existiert' do
-    a = make_referee(lizenznummer: 20006)
+    a = make_referee(lizenznummer: 20_006)
 
     assert_nothing_raised do
-      a.update!(partner_lizenznummer: 88888)
+      a.update!(partner_lizenznummer: 88_888)
     end
-    assert_equal 88888, a.reload.partner_lizenznummer
+    assert_equal 88_888, a.reload.partner_lizenznummer
   end
 
   test 'partner_lizenznummer: Self-Reference setzt keinen Gegen-Eintrag' do
-    a = make_referee(lizenznummer: 20007)
+    a = make_referee(lizenznummer: 20_007)
 
     assert_nothing_raised do
       a.update!(partner_lizenznummer: a.lizenznummer)
